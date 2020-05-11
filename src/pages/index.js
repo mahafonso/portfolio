21a38402c2986ad97f3864e0048c26203dc8b509
Marcela Afonso <marcela.afonso@grupoboticario.com.br>
2020-05-11 15:27:07 -0300

    Add: title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import Home from './Home/HomeComponent';
 
 import './style.scss';
 
-export default () => <Home></Home>
+export default () => 
+    <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Marcela Braga Afonso - FrontEnd WebDeveloper</title>
+        </Helmet>
+        
+        <Home></Home>
+    </>
