d11c23e7f9ebd4730afe2d0c6ef1118b36a1af79
Marcela Afonso <marcela.afonso@grupoboticario.com.br>
2020-05-11 15:41:31 -0300

    Add: function to calculate and automatically update the age

--- src/pages/Home/HomeComponent.js
+++ src/pages/Home/HomeComponent.js
@@ -5,38 +5,51 @@ import Footer from '../components/Footer/FooterComponent';
 
 import './HomeComponentStyle.scss';
 
-export default () => (
-    <>
-        <Header></Header>
+export default () => {
+    const age = calculateAge();
 
-        <section class="content">
-            <div class="content__wrapper">
-                <p class="wrapper__title">Olá, seja bem-vindo(a)!</p>
+    return (
+        <>
+            <Header></Header>
 
-                <p class="wrapper__text">
-                    Meu nome é Marcela Braga Afonso, tenho 29 anos e sou programadora front end desde 2013.
-                    Escolhi estudar Programação porque sempre gostei muito de tecnologia e queria entender como as coisas funcionavam, eram construídas. E me apaixonei pelo desenvolvimento web desde o começo, pela forma como é dinâmico, como você consegue ver as coisas se desenvolvendo conforme vai fazendo.
-                </p>
+            <section class="content">
+                <div class="content__wrapper">
+                    <p class="wrapper__title">Olá, seja bem-vindo(a)!</p>
 
-                <p class="wrapper__text">
-                    Profissionalmente trabalhei em startup, agência de publicidade e e-commerces. Trabalhei com várias tecnologias e pude aprender bastante além do básico HTML e CSS: JavaScript, jQuery, React, Angular, TypeScript, Bootstrap, SASS, LESS, styled-components, PhoneGap, Gulp, Babel. Para versionamento, SVN e Git (Bitbucket, GitHub e GitLab). E grande experiência com a metodologia ágil SCRUM.
-                    <br />
-                    Trabalhei para clientes como Whirlpool (CompraCerta, Brastemp, Consul, KitchenAid), B.blend, Itau, Terra, além de nas empresas Marcyn, Avec e Boticário (onde estou atualmente). Definitivamente minha maior experiência foi com e-commerce, utilizando a plataforma Vtex, mas também já desenvolvi sites institucionais, landing pages e e-mails marketing, além de uma implementação de chatbot utilizando API de terceiros para gerar perguntas e respostas.
-                    <br />
-                    Tive a oportunidade de dar suporte à equipe de CRO, desenvolvendo alguns Testes AB e automatizando a coleta e exibição de dados do Google Analytics em tabelas e gráficos no Google Sheet para melhor apresentação e análise.
-                </p>
+                    <p class="wrapper__text">
+                        Meu nome é Marcela Braga Afonso, tenho {age} anos e sou programadora front end desde 2013.
+                        Escolhi estudar Programação porque sempre gostei muito de tecnologia e queria entender como as coisas funcionavam, eram construídas. E me apaixonei pelo desenvolvimento web desde o começo, pela forma como é dinâmico, como você consegue ver as coisas se desenvolvendo conforme vai fazendo.
+                    </p>
 
-                <p class="wrapper__text">
-                    Na área de tecnologia tudo está sempre mudando e surgindo coisas novas, então procuro sempre me atualizar e ter uma visão geral sobre o que está acontecendo, sobre as novas tendências. Para isso, faço alguns cursos ou me aventuro em alguma linguagem ou tecnologia nova, desenvolvendo sites pessoais. Apesar de não usar profissionalmente, procuro aprender fazendo coisas pra mim. Esse site foi desenvolvido com Gatsby e desenvolvi esse (site) em Next.js.
-                </p>
+                    <p class="wrapper__text">
+                        Profissionalmente trabalhei em startup, agência de publicidade e e-commerces. Trabalhei com várias tecnologias e pude aprender bastante além do básico HTML e CSS: JavaScript, jQuery, React, Angular, TypeScript, Bootstrap, SASS, LESS, styled-components, PhoneGap, Gulp, Babel. Para versionamento, SVN e Git (Bitbucket, GitHub e GitLab). E grande experiência com a metodologia ágil SCRUM.
+                        <br />
+                        Trabalhei para clientes como Whirlpool (CompraCerta, Brastemp, Consul, KitchenAid), B.blend, Itau, Terra, além de nas empresas Marcyn, Avec e Boticário (onde estou atualmente). Definitivamente minha maior experiência foi com e-commerce, utilizando a plataforma Vtex, mas também já desenvolvi sites institucionais, landing pages e e-mails marketing, além de uma implementação de chatbot utilizando API de terceiros para gerar perguntas e respostas.
+                        <br />
+                        Tive a oportunidade de dar suporte à equipe de CRO, desenvolvendo alguns Testes AB e automatizando a coleta e exibição de dados do Google Analytics em tabelas e gráficos no Google Sheet para melhor apresentação e análise.
+                    </p>
 
-                <p class="wrapper__text">
-                    Além da paixão por tecnologia, também sou apaixonada por dogs, viagens e idiomas! Adoro conhecer novos lugares, culturas, costumes e histórias.
-                    Já conheci alguns países, aprendi inglês, italiano e estou aprendendo francês :)
-                </p>
-            </div>
-        </section>
+                    <p class="wrapper__text">
+                        Na área de tecnologia tudo está sempre mudando e surgindo coisas novas, então procuro sempre me atualizar e ter uma visão geral sobre o que está acontecendo, sobre as novas tendências. Para isso, faço alguns cursos ou me aventuro em alguma linguagem ou tecnologia nova, desenvolvendo sites pessoais. Apesar de não usar profissionalmente, procuro aprender fazendo coisas pra mim. Esse site foi desenvolvido com Gatsby e desenvolvi esse (site) em Next.js.
+                    </p>
 
-        <Footer></Footer>
-    </>
-);
+                    <p class="wrapper__text">
+                        Além da paixão por tecnologia, também sou apaixonada por dogs, viagens e idiomas! Adoro conhecer novos lugares, culturas, costumes e histórias.
+                        Já conheci alguns países, aprendi inglês, italiano e estou aprendendo francês :)
+                    </p>
+                </div>
+            </section>
+
+            <Footer></Footer>
+        </>
+    );
+}
+
+const calculateAge = () => {
+    const birthDate = new Date('8-17-1990').getTime(),
+        currentDate = new Date().getTime(),
+        difference = currentDate - birthDate,
+        years = difference / 31536000000;
+
+    return parseInt(years);
+}
