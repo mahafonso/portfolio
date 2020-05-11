a15df8ff0cceb9f9456a3ffb93a836eaaa67a98f
Marcela Afonso <marcela.afonso@grupoboticario.com.br>
2020-05-11 15:30:02 -0300

    Add: title

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default () =>
     <>
         <Helmet>
             <meta charSet="utf-8" />
-            <title>Marcela Braga Afonso - FrontEnd WebDeveloper</title>
+            <title>Marcela Braga Afonso - FrontEnd Web Developer</title>
         </Helmet>
         
         <Home></Home>
